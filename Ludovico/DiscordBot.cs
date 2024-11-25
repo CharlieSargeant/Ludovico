af933a89f218fce6ffa0ef7060ea12d40f6ebd36
CharlieSargeant <103066953@student.swin.edu.au>
2024-11-25 19:26:42 +1100

    Added bot connection, message sending, and message receiving functionality.

---
+++ Ludovico/DiscordBot.cs
@@ -0,0 +1,51 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Ludovico
+{
+    public class DiscordBot
+    {
+        public DiscordSocketClient Client { get; private set; }
+        private string _botToken;
+
+        public event Func<LogMessage, Task> LogReceived;
+        public event Func<SocketMessage, Task> MessageReceived;
+        public event Func<Task> Ready;
+
+        public DiscordBot(string token)
+        {
+            _botToken = token;
+
+            var config = new DiscordSocketConfig
+            {
+                GatewayIntents = GatewayIntents.Guilds |
+                                 GatewayIntents.GuildMessages |
+                                 GatewayIntents.GuildMessageReactions |
+                                 GatewayIntents.DirectMessages |
+                                 GatewayIntents.MessageContent
+            };
+
+            Client = new DiscordSocketClient(config);
+
+            // Subscribe to client events and forward them
+            Client.Log += log => LogReceived?.Invoke(log);
+            Client.MessageReceived += msg => MessageReceived?.Invoke(msg);
+            Client.Ready += () => Ready?.Invoke();
+        }
+
+        public async Task ConnectAsync()
+        {
+            await Client.LoginAsync(TokenType.Bot, _botToken);
+            await Client.StartAsync();
+        }
+
+        public async Task DisconnectAsync()
+        {
+            await Client.LogoutAsync();
+            await Client.StopAsync();
+        }
+    }
+}
--- Ludovico/MainWindow.xaml.cs
+++ Ludovico/MainWindow.xaml.cs
@@ -1,24 +1,126 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Discord;
+using Discord.WebSocket;
 
 namespace Ludovico
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private DiscordBot _bot;
+        private bool _isConnected = false;
+
+        // Guild and channel data
+        private List<KeyValuePair<string, ulong>> _guilds = new();
+        private Dictionary<ulong, List<ITextChannel>> _guildChannels = new();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            _bot = new DiscordBot("Bot_Token");
+            _bot.LogReceived += LogAsync;
+            _bot.MessageReceived += MessageReceivedAsync;
+            _bot.Ready += BotReadyAsync;
+
+            GuildComboBox.SelectionChanged += GuildComboBox_SelectionChanged;
+        }
+
+        private async void ConnectButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!_isConnected)
+            {
+                await _bot.ConnectAsync();
+                _isConnected = true;
+                ConnectButton.Content = "Disconnect Bot";
+            }
+            else
+            {
+                await _bot.DisconnectAsync();
+                _isConnected = false;
+                ConnectButton.Content = "Connect Bot";
+            }
+        }
+
+        private async void SendMessageButton_Click(object sender, RoutedEventArgs e)
+        {
+            string message = MessageTextBox.Text;
+            if (!string.IsNullOrEmpty(message) && _isConnected)
+            {
+                var selectedChannelId = ChannelComboBox.SelectedValue?.ToString();
+                if (!string.IsNullOrEmpty(selectedChannelId))
+                {
+                    await SendMessageAsync(selectedChannelId, message);
+                    MessageTextBox.Clear();
+                }
+            }
+        }
+
+        private Task LogAsync(LogMessage log)
+        {
+            Dispatcher.Invoke(() => LogTextBox.AppendText(log.Message + "\n"));
+            return Task.CompletedTask;
+        }
+
+        private Task MessageReceivedAsync(SocketMessage message)
+        {
+            if (message.Author.IsBot) return Task.CompletedTask;
+            Dispatcher.Invoke(() => LogTextBox.AppendText($"Message received: {message.Content}\n"));
+            return Task.CompletedTask;
+        }
+
+        private Task BotReadyAsync()
+        {
+            Dispatcher.Invoke(() =>
+            {
+                _guilds.Clear();
+                _guildChannels.Clear();
+
+                foreach (var guild in _bot.Client.Guilds)
+                {
+                    _guilds.Add(new KeyValuePair<string, ulong>(guild.Name, guild.Id));
+                    var channels = new List<ITextChannel>(guild.TextChannels);
+                    _guildChannels[guild.Id] = channels;
+                }
+
+                GuildComboBox.ItemsSource = _guilds;
+                GuildComboBox.DisplayMemberPath = "Key";
+                GuildComboBox.SelectedValuePath = "Value";
+            });
+            return Task.CompletedTask;
+        }
+
+        private void GuildComboBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            if (GuildComboBox.SelectedValue is ulong guildId)
+            {
+                ChannelComboBox.ItemsSource = _guildChannels.TryGetValue(guildId, out var channels) ? channels : null;
+                ChannelComboBox.DisplayMemberPath = "Name";
+                ChannelComboBox.SelectedValuePath = "Id";
+            }
+        }
+
+        private async Task SendMessageAsync(string channelId, string message)
+        {
+            try
+            {
+                var channel = _bot.Client.GetChannel(ulong.Parse(channelId)) as ITextChannel;
+                if (channel != null)
+                {
+                    await channel.SendMessageAsync(message);
+                    LogTextBox.AppendText($"Sent message: {message}\n");
+                }
+                else
+                {
+                    LogTextBox.AppendText("Error: Channel not found.\n");
+                }
+            }
+            catch (Exception ex)
+            {
+                LogTextBox.AppendText($"Error: {ex.Message}\n");
+            }
         }
     }
 }
